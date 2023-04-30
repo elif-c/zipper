28562fbcca093259acff950549ce199500a9cc9e
elif c <86270311+elif-c@users.noreply.github.com>
2023-04-30 20:18:16 +0300

    UI changes

--- zipping_gui.py
+++ zipping_gui.py
@@ -2,7 +2,9 @@ import shutil
 import PySimpleGUI
 from zipping import make_archive
 
-select_files = PySimpleGUI.Text("Select files to zip:")
+PySimpleGUI.theme("DarkGreen4")
+
+select_files = PySimpleGUI.Text("Select files for zipping:")
 input1 = PySimpleGUI.Input()
 choose_button1 = PySimpleGUI.FilesBrowse("Browse", key="selected_files")
 
@@ -12,12 +14,15 @@ choose_button2 = PySimpleGUI.FolderBrowse("Browse", key="selected_folder")
 
 name_file = PySimpleGUI.InputText(".zip", key="name_file")
 
-zip_button = PySimpleGUI.Button("Zip", button_color=("white", "purple"))
-message = PySimpleGUI.Text(key="message", text_color="purple")
+zip_button = PySimpleGUI.Button("Zip", button_color=("white", "purple"), size=(6, 1))
+message = PySimpleGUI.Text(key="message", text_color="DarkOrchid1", justification="right", font="Futura 11 bold")
 
-window = PySimpleGUI.Window("File Zipper", layout=[[select_files, input1, choose_button1],
-                                                   [select_destinations, input2, choose_button2],
-                                                   [name_file, zip_button, message]])
+layout = [[select_files, PySimpleGUI.Push(), input1, choose_button1],
+          [select_destinations, input2, choose_button2],
+          [PySimpleGUI.Push(), name_file, zip_button],
+          [PySimpleGUI.Push(), message]]
+
+window = PySimpleGUI.Window("File Zipper", layout=layout, default_button_element_size=(6, 1))
 while True:
     event_key, value = window.read()
 
